359193ed6a87ef3875ddb0e0947f671186cf1024
HanaMartinkovaKuchynkova <h.m.kuchynkova@gmail.com>
2024-02-13 22:52:01 +0100

    Vytvořeny třídy.

--- src/main/java/cz/czechitas/ukol3/HlavniProgram.java
+++ src/main/java/cz/czechitas/ukol3/HlavniProgram.java
@@ -7,7 +7,26 @@ public class HlavniProgram {
 
     public static void main(String... args) {
         //TODO tady bude váš kód - vytvoření instance třídy Pocitac, zapnutí, vpynutí, výpis hodnot.
-        System.out.println("Program spuštěn.");
+        //System.out.println("Program spuštěn.");
+
+        Disk mujDisk = new Disk();
+        mujDisk.setKapacita(994_662_584_320L);
+        mujDisk.setVyuziteMisto(12_000_000_000L);
+
+        Pamet mojePamet = new Pamet();
+        mojePamet.setKapacita(24_000_000_000L);
+
+        Procesor mujProcesor = new Procesor();
+        mujProcesor.setRychlost(3_490_000_000L);
+        mujProcesor.setVyrobce("HP");
+
+        System.out.println(mujDisk.getKapacita());
+        System.out.println(mujDisk.getVyuziteMisto());
+
+        System.out.println(mojePamet.getKapacita());
+
+        System.out.println(mujProcesor.getRychlost());
+        System.out.println(mujProcesor.getVyrobce());
     }
 
 }
---
+++ src/main/java/cz/czechitas/ukol3/Pocitac.java
@@ -0,0 +1,16 @@
+package cz.czechitas.ukol3;
+
+public class Pocitac {
+
+  /*  public boolean jeZapnuty() {
+        return false;
+    }
+
+    public void zapniSe() {
+
+    }
+
+    public void vypniSe();*/
+
+
+}
