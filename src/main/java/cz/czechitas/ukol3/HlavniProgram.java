b667c117879866049ea1aaade85e196928ed1b64
HanaMartinkovaKuchynkova <h.m.kuchynkova@gmail.com>
2024-02-23 08:42:51 +0100

    Hotové obě části úkolu.

--- src/main/java/cz/czechitas/ukol3/HlavniProgram.java
+++ src/main/java/cz/czechitas/ukol3/HlavniProgram.java
@@ -8,10 +8,10 @@ public class HlavniProgram {
     public static void main(String... args) {
         //TODO tady bude váš kód - vytvoření instance třídy Pocitac, zapnutí, vpynutí, výpis hodnot.
 
-
+        // Demonstrace 1. úkolu
         Pocitac mujPocitac = new Pocitac();
         System.out.println(mujPocitac.toString());
-        mujPocitac.zapniSe();
+        mujPocitac.zapniSe(); // Vypíše chybu, protože PC v tuto chvíli nemá všechny povinné součáti.
 
         Procesor mujProcesor = new Procesor();
         mujProcesor.setRychlost(3_490_000_000L);
@@ -31,15 +31,16 @@ public class HlavniProgram {
         System.out.println(mujPocitac.toString());
 
         mujPocitac.zapniSe();
-        mujPocitac.zapniSe();
+        mujPocitac.zapniSe(); // vypíše chybu, protože PC už běží
         System.out.println(mujPocitac.toString());
         mujPocitac.vypniSe();
 
-        mujPocitac.vypniSe();
+        mujPocitac.vypniSe(); // Nevypíše chybu a ani nic neprovede, protože PC je už vypnutý.
 
         /*
         // Demonstrace 2. úkolu
         // Vytvoření souboru
+        mujPocitac.zapniSe();
         mujPocitac.vytvorSouborOVelikosti(662_584_500L); // Vypíše chybovou hlášku Nedostatek místa na disku!
 
         mujPocitac.vytvorSouborOVelikosti(662_584_300L); // Vypíše hlášku Soubor se vytvořil.
@@ -48,6 +49,7 @@ public class HlavniProgram {
         mujPocitac.vytvorSouborOVelikosti(662_584_300L); // Vypíše chybovou hlášku Nelze provést. Pocítač je vypnutý.
 
         // Odstranění souboru
+        mujPocitac.zapniSe();
         mujPocitac.vymazSouborOVelikosti(999_000_000_000L); // Vypíše chybovou hlášku Chyba. Využité místo nesmí klesnout pod 0
 
         mujPocitac.vymazSouborOVelikosti(662_584_300L); // Vypíše hlášku Soubor byl odstraněn.
